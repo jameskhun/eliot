9eef99d6a5f8a8eeda93d58cb2dc8c80082e89a7
lauderdale <b.e.lauderdale@lse.ac.uk>
2015-02-14 15:12:15 +0000

    more changes

--- R/fadeColour.R
+++
@@ -1,6 +0,0 @@
-# Function to fade a colour by specified alpha
-	
-fade.colour <- function(colours,alpha=1){
-	rgb.dat <- col2rgb(colours)
-	return(rgb(rgb.dat[1,],rgb.dat[2,],rgb.dat[3,],alpha*255,maxColorValue=255))
-	}	
--- R/standardize.R
+++ R/standardize.R
@@ -1,15 +1,7 @@
-# Function to subtract mean and divide by standard deviation
-
-standardize <- function(x) return((x-mean(x,na.rm=TRUE))/sd(x,na.rm=TRUE))
-
-# Apply standardize within subgroups defined by "by"
-
-standardize.by <- function(x,by){
-	by <- as.factor(by)
-	by.levels <- levels(by)
-	out <- rep(NA,length(x))
-	for (i in 1:length(by.levels)){
-		out[as.numeric(by) == i] <- standardize(x[as.numeric(by) == i])
-	}
-	return(out)	
-	}
+#' Function to standardize a vector
+#' 
+#' 
+#' \code{standardize} subtracts the mean from and divides by the standard deviation of a vector.
+#' @param x 
+#' @export
+standardize <- function(x,na.rm=FALSE) return((x-mean(x,na.rm=na.rm))/sd(x,na.rm=na.rm))
